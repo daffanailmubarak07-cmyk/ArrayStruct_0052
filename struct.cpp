69fa4010ad3e009ec9dd4057656d218d3b7b87b5
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-04-07 15:54:01 +0700

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -10,3 +10,13 @@ struct orang
 
 int main()
 {
+// deklarasi object dari stuct orang
+    orang mhs;
+// isi object
+    cout << "nama mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "alamat asal : ";
+    cin >> mhs.alamat;
+    cout << "umur : ";
+    cin >> mhs.umur;
+   
