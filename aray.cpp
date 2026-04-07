86359f8cd8c4e3fe099eac97cc18d5cf2eda2ad7
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-04-07 15:48:56 +0700

    Update aray.cpp

--- aray.cpp
+++ aray.cpp
@@ -44,4 +44,13 @@ int main(){
         cin >> datamu[i];
     }
 
+cout <<endl;
+
+//menampilkan datamu dg perulangan for
+//menampilkan datamu dg perulangan for
+    for (int i = 0; i <= 4; i++)
+   {
+    cout << "data ke - " << i + 1 << " : " << datamu[i] <<endl;
+   }
+}
    
